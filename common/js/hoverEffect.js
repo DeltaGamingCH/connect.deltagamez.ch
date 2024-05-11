15340cf9e39bed04b7a3d44e96b50ac8e28c5271
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-05-11 14:57:58 +0200

    Highlight effect incomplete, added comments

--- common/js/hoverEffect.js
+++ common/js/hoverEffect.js
@@ -1,13 +1,13 @@
 let link = document.getElementById('custom-link');
 
-// Add event listener for mouseover event
 link.addEventListener('mouseover', function() {
-    // Change the color to red on mouseover
     link.style.color = 'red';
 });
 
-// Add event listener for mouseout event
+
 link.addEventListener('mouseout', function() {
-    // Change the color back to the default color on mouse release
-    link.style.color = ''; // Reset to default color
+
+    link.style.color = '';
 });
+
+console.log("loaded");
