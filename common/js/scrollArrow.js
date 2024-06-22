aa2a123e95ff46bc55d474357eb0f5c9b6ba4eff
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-06-22 10:59:37 +0200

    Removed comments, minor scroll-behavior changes

--- common/js/scrollArrow.js
+++ common/js/scrollArrow.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const item = document.querySelector('.header-footer-scroll');
-    const container = document.querySelector('.container');
+    const container = document.querySelector('.main-header');
 
     item.addEventListener('click', function() {
         container.scrollIntoView({ behavior: 'smooth', block: 'start' });
